1a893d7fdffbf09624aae4ce426a1a143757a90a
Oscar León <oscarleon.alcatel@gmail.com>
2022-08-23 08:39:01 -0600

    creando la constante para generar el pdf

--- v1.js
+++ v1.js
@@ -7,4 +7,9 @@ function createPDF(){
     const tempFolder = DriveApp.getFolderById("1AOiNVaVydND5CfIZLcMPtH_JyyjEIpCe"); // Agregar el ID del URL
     const templateDoc = DriveApp.getFileById("1pKZrM0x_plj1Cc1SEELvbrFHDePta9OP5bddDII8EKw"); // Agregar el ID del URL
 
+    const newTempFile = templateDoc.makeCopy(tempFolder); // Crea una copia temporal de la plantilla
+
+    const blobPDF = newTempFile.getAs(MimeType.PDF);
+    const pdfFile = pdfFolder.createFile(blobPDF).setName("My PDF");
+
 }
