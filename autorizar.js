28c913f9066e2dbdda7ed623a0656699f39419ac
Oscar León <oscarleon.alcatel@gmail.com>
2022-08-23 08:18:14 -0600

    se inicializan las variables para modificar el drive

--- autorizar.js
+++
@@ -1,9 +0,0 @@
-function onOpen(e){
-    var ssId = SpreadsheetApp.getActiveSpreadsheet().getId();
-    
-    ScriptApp.newTrigger("makeDropDownMenu")
-        .forSpreadsheet(ssId)
-        .onOpen()
-        .create();
-    
-    }
---
+++ v1.js
@@ -0,0 +1,10 @@
+//Este es el código con el cual se inicializan las variables para convetir los PDF
+
+function createPDF(){
+
+    //Se declaran las variables para manipular el Drive
+    const pdfFolder = DriveApp.getFolderById("1Hx2uyKM5vOswhsdJeCYYuJcwjl5JJTGH"); // Agregar el ID del URL
+    const tempFolder = DriveApp.getFolderById("1AOiNVaVydND5CfIZLcMPtH_JyyjEIpCe"); // Agregar el ID del URL
+    const templateDoc = DriveApp.getFileById("1pKZrM0x_plj1Cc1SEELvbrFHDePta9OP5bddDII8EKw"); // Agregar el ID del URL
+
+}
